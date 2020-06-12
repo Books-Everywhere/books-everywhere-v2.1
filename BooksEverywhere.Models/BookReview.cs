c97f4911c4469487c3ba548c39049ca7a9469919
luizous <luizaruivoms@gmail.com>
2020-06-11 23:12:26 -0300

    (feat/book-review): created book review model and map

---
+++ BooksEverywhere.Models/BookReview.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksEverywhere.Models
+{
+    public class BookReview
+    {
+        public virtual Book Book { get; set; }
+        public virtual User User { get; set; }
+        public virtual string Review { get; set; }
+    }
+}
---
+++ BooksEverywhere.Repositories/Mappings/BookReviewMap.cs
@@ -0,0 +1,16 @@
+﻿using BooksEverywhere.Models;
+using FluentNHibernate.Mapping;
+
+namespace BooksEverywhere.Repositories.Mappings
+{
+    public class BookReviewMap : ClassMap<BookReview>
+    {
+        public BookReviewMap()
+        {
+            Table("book_review");
+            References(x => x.Book).Column("id").Not.Nullable();
+            References(x => x.User).Column("id").Not.Nullable();
+            Map(x => x.Review, "review").Not.Nullable();
+        }
+    }
+}
